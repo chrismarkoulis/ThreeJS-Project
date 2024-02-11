97f85303a85763874ba214c9c38f20ca874e48d8
Christos Markoulis <cmarkoulis@epignosishq.com>
2024-02-11 22:56:37 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/', // Set the base URL to root
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.ico'], // Include image formats
   build: {
-    outDir: 'dist'
-  },
-  assetsInclude: ['assets']
+    outDir: 'dist',
+    assetsDir: 'assets',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: './index.html' // Entry point
+      }
+    }
+  }
 });
